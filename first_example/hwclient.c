1b91b462bd3e7c29c87a3e7819611c8dba167a37
linh <mrlinhnv2801@gmail.com>
2021-11-01 22:07:39 +0700

    change hwclient.c add argument

--- first_example/hwclient.c
+++ first_example/hwclient.c
@@ -9,7 +9,7 @@ int main (void)
     printf ("Connecting to hello world server...\n");
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:5555");
+    zmq_connect (requester, "tcp://172.17.0.2:5555");
 
     int request_nbr;
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
